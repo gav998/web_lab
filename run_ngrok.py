dea8eb7c64e25fc5e02d9f8ebf2462c9eede5a1e
gav998 <gav998@yandex.ru>
2023-10-01 01:10:20 +0300

    fig bug for win

--- run_ngrok.py
+++ run_ngrok.py
@@ -9,16 +9,16 @@ from sys import platform
 def start_tunnels():
     print("start_tunnels")
     if "win" in platform:
-        proc = subprocess.Popen("ngrok http 9090", shell=True)
+        proc = subprocess.Popen("start ngrok http 9090", shell=True)
     else:
         proc = subprocess.Popen("lxterminal -e ngrok http 9090", shell=True)
     time.sleep(5)
-    update_conf(get_tunnels())
     return proc
 
-def get_tunnels():
-    print("get_tunnels")
-    os.system("curl  http://localhost:4040/api/tunnels > ./tunnels.json")
+def get_public_url():
+    print("get_public_url")
+    os.system("curl http://localhost:4040/api/tunnels > ./tunnels.json")
+    
 
     with open('./tunnels.json', 'rt') as data_file:
         j = data_file.read()
@@ -32,7 +32,6 @@ def get_tunnels():
     else:
         print('tunnels.json unknown')
         return False
-
 
 
 '''
@@ -51,29 +50,33 @@ def restart_tunnels():
     proc.terminate()
     time.sleep(5)
     proc = start_tunnels()
+    public_url = get_public_url()
+    update_conf(public_url)
     time.sleep(15)
 
 
-def update_conf(tunnels):
+def update_conf(public_url):
     print("update_conf")
     with open('./client/conf.js', "a") as conf_file:
         conf_file.write(f"// {datetime.datetime.today()}\n")
-        conf_file.write(f'server = "{tunnels}"\n\n')
+        conf_file.write(f'server = "{public_url}"\n\n')
     if "win" in platform:
-        os.system("python ./upload_conf_ftp.py")
+        os.system("start python ./upload_conf_ftp.py")
     else:
         os.system("lxterminal -e python3 ./upload_conf_ftp.py")
 
 
 proc = start_tunnels()
+public_url=get_public_url() 
+update_conf(public_url)
 time.sleep(5)
 
 while True:
-    tunnels = get_tunnels()
-    print(tunnels)
-    if not tunnels:
-        print(datetime.datetime.today(), "tunnels NOT EXIST")
+    public_url = get_public_url()
+    # print(public_url)
+    if not public_url:
+        print(datetime.datetime.today(), "public_url NOT EXIST")
         restart_tunnels()
     else:
-        print(datetime.datetime.today(), "OK", tunnels)
+        print(datetime.datetime.today(), "OK", public_url)
         time.sleep(10)
---
+++ start_win.bat
@@ -0,0 +1,2 @@
+start cmd.exe /k "conda activate ./venv && python run_ngrok.py"
+start cmd.exe /k "conda activate ./venv && cd server && python server.py"
